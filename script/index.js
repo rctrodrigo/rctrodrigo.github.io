436f5519fdeb0c4602a55afdfac0596573acc547
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-02-25 13:46:10 +0800

    feat(navigation): modify navigation links using array and template literals

--- script/index.js
+++ script/index.js
@@ -1,10 +1,33 @@
+// Brand name
+const siteName = "Raven Rodrigo";
+
+/* Objects in Array */
+const navigationLinks = [
+    { name: 'Home', link: '#' },
+    { name: 'About', link: '#' },
+    { name: 'Blog', link: '#' },
+    { name: 'Projects', link: '#' },
+    { name: 'Tools',  link: '#' },
+    { name: 'Contact', link: '#' }
+];
+
+/* Array.map function and Template Literals */ 
+const navBarMenu = `
+${navigationLinks
+    .map(navLinks => `<li class="navbar-items">
+        <a href="#" class="navbar-link">${navLinks.name}</a>`).join(' ')}</li>
+`;
+
+document.querySelector('ul').innerHTML = navBarMenu;
+
+
 // open modal
 function openModal() {
     document.getElementById('modal-resume').classList.add('open');
     document.body.classList.add('modal-resume-open');
 }
 
-// cloase currently open modal
+// close currently open modal
 function closeModal() {
     document.querySelector('.modal-resume.open').classList.remove('open');
     document.body.classList.remove('modal-resume-open');
@@ -17,4 +40,5 @@ window.addEventListener('load', function() {
             closeModal();
         }
     })
-})
+});
+
